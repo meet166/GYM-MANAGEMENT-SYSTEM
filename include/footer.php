f3b9e6e94750c99af9bb66c417ca3886ccf2ec46
meet166 <meetptl166@gmail.com>
2024-04-12 13:16:03 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">	 
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
